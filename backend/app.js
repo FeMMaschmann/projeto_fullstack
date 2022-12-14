18f7ba80017d8bb685ad3024f2a966e4e9b6c88e
Fernando Mottin Maschmann <fernando.maschmann@outlook.com>
2022-12-13 21:53:21 -0300

    add cors

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const app = express();
 const porta = 5000;
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const bookRouter = require("./routes/books");
 const authorRouter = require("./routes/authors");
 const clientRouter = require("./routes/clients");
 const withdrawalRouter = require("./routes/withdrawals");
 const loginRouter = require("./routes/logins");
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/api/books", bookRouter);
 app.use("/api/authors", authorRouter);
